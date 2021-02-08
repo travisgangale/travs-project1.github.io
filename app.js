70bfeddc21e1711fa7dd3f9787f52936364a8101
travisgangale <travisgangale@gmail.com>
2021-02-08 13:56:30 -0500

    changed my way of going about boxes to make it simplier to add text to each box visually

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 // All boxes with price and categories
 //////////////////////////////////////
 
+
+
+// Have to put all the words and prices in boxes now
+///////////////////////////////////////////////////
 $(() => {
-    for (let i = 0; i < 30; i++) {
-        const $boxes = $('<div>');
-        $boxes.addClass('square');
-        $('#container').append($boxes);
-    }
+    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    // const arr = ['food', 'programming', 'math', 'history', 'states']
+    
+    
+    
+    
+    // for (let i = 0; i < 30; i++) {
+    //     const $boxes = $('<div>');
+    //     $boxes.addClass('square');
+    //     const $ul = $('<ul>');
+        
+    //     $ul.append($('<li>').text(arr));
+        
+    //     $boxes.append($ul);
+    //     $('#container').append($boxes);
+    // }
+
+
+
+
 
 
 
