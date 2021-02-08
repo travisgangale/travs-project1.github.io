8f94602e6c79cf8c274f18c9f0c31455ef4a37e7
travisgangale <travisgangale@gmail.com>
2021-02-08 11:35:05 -0500

    have boxes created for jeopardy just figuring out how to wrap correctly as needed for my game

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-console.log('hello')
+// All boxes with price and categories
+//////////////////////////////////////
+
+$(() => {
+    for (let i = 0; i < 30; i++) {
+        const $boxes = $('<div>');
+        $boxes.addClass('square');
+        $('#container').append($boxes);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
