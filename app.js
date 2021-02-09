4a0640f4362d8e3e75d235f93de0c0b939d19833
travisgangale <travisgangale@gmail.com>
2021-02-09 13:15:32 -0500

    figuring out the modal input and submit

--- app.js
+++ app.js
@@ -13,24 +13,43 @@ $(() => {
     const $question = $('.square').on('click', (event) => {
         $(event.currentTarget).css('text-decoration', 'line-through');
         
-    })
-
+    
+// creation of the modal for the questions when clicked
     const $modalBtn = $('#modal-btn');
     const $modal = $('.modal');
     const $closeBtn = $('.close-btn');
+    const $submitBtn = $('#submit');
+    const $inputText = $('#inputText');
+
+
 
     $modalBtn.on('click', () => {
         $modal.css('display', 'block');
     })
+
+
     $closeBtn.on('click', () => {
         $modal.css('display', 'none');
     })
-    window.on('click', (e) => {
+
+
+    $submitBtn.on('click', () => {
+        if ($inputText == true) {
+            alert('Correct!');
+        } else {
+            alert('incorrect');
+        }  
+    })
+    
+
+    $(window).on('click', (e) => {
         if (e.target == $modal) {
             $modal.css('display', 'none');
         }
     })
+})
    
+
    
    
    
