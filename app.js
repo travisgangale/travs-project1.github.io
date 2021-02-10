96eeecd8b0a5d908ba8eab4b13cb57928921e3cb
travisgangale <travisgangale@gmail.com>
2021-02-10 18:13:19 -0500

    Figured out how to assign names for all of the answers now im givving answers to all the questions 2 per for now to test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 $(() => {
     // where the questions come from...
     const arrQuestion = ['I am used to make Guacamole', 'monkeys peel me backwards', 'I have cheese an im a triangle', 'I am usually falling from a tree and im round', 'I am usually made from cows']
-// ANSWERS: What is a avocato?, What is a banana?, What is pizza?, What is a coconut?, What is steak?...
+// ANSWERS: What is an avocato?, What is a banana?, What is pizza?, What is a coconut?, What is steak?...
     const arrQuestion2 = ['Steps used to complete a task', 'The  act of going through an array over and over', 'programs need this when parameters are met', 'containers for storing values', 'A list of values or strings']
 // ANSWERS: What is an algorithm?, What is looping?, What are conditionals?, What are variables?, What is an array?...
     const arrQuestion3 = ['6 x 7', '2 x 5 - 3', 'parenthesis, exponents, multiplication, division, addition, subtraction', '2 lines that run the same way but never touch', 'An angle more than 90 degrees but less than 180 degrees']
@@ -63,27 +63,118 @@ $(() => {
         'fourty5': arrQuestion5[3],
         'fifty5': arrQuestion5[4]
     }
+
+///////// The Answers being pulled into the obj from the array /////////
+
+    const answerFood1 = ['What is an avocato?', 'What is a potato?', 
+    'What is a banana?', 'What is an Orange?',
+    'What is pizza?', 'What is sausage?',
+    'What is a coconut?', 'What is a bird?',
+    'What is steak?', 'What is grass?']
+    const objAnswerFood = {
+        'ten1': answerFood1[0],
+        'twenty1': answerFood1[2],
+        'thirty1': answerFood1[4],
+        'fourty1': answerFood1[6],
+        'fifty1': answerFood1[8],
+    }
+    const objAnswerFood1 = {
+        'ten1': answerFood1[1],
+        'twenty1': answerFood1[3],
+        'thirty1': answerFood1[5],
+        'fourty1': answerFood1[7],
+        'fifty1': answerFood1[9],
+    }
     
+    
+    // ANSWERS: What is an algorithm?, What is looping?, What are conditionals?, What are variables?, What is an array?
+    const answerProgramming = ['What is an Algorithm?', 'What is a Boolean?']
+    const objAnswerProgramming = {
+        'ten2': answerProgramming[0],
+    }
+    const objAnswerProgramming1 = {
+        'ten2': answerProgramming[1],
+    }
+
+
+
+    const answerMath = ['What is 42?', 'What is 52?']
+    const objAnswerMath = {
+        'ten3': answerMath[0],
+    }
+    const objAnswerMath1 = {
+        'ten3': answerMath[1],
+    }
+
+
+
+    const answerHistory = ['Who is Christopher Columbus?', 'Who is Freddy Mercury?']
+    const objAnswerHistory = {
+        'ten4': answerHistory[0],
+    }
+    const objAnswerHistory1 = {
+        'ten4': answerHistory[1],
+    }
+
+
+
+    const answerStates = ['Where is the pentagon?', 'Where is the Miracle Building of Arts?']
+    const objAnswerStates = {
+        'ten5': answerStates[0],
+    }
+    const objAnswerStates1 = {
+        'ten5': answerStates[1],
+    }
+
+
+
+//////// What is going to make everything work ////////
     const $question = $('.square').on('click', (event) => {
         $(event.currentTarget).css('text-decoration', 'line-through')
         $modal.css('display', 'block');
         
         // taking the blank text from the modal and replacing it with what you stored in obj...
-
+        // The questions //
         $('#modalText').text(objFood[event.currentTarget.id])
         $('#modalText').text(objProgramming[event.currentTarget.id])
         $('#modalText').text(objMath[event.currentTarget.id])
         $('#modalText').text(objHistory[event.currentTarget.id])
         $('#modalText').text(objStates[event.currentTarget.id])
 
+
+
+
+
+        //inputing text into the buttons
+//Food Answer Buttons
+        $('#submit').text(objAnswerFood[event.currentTarget.id])
+        $('#submit1').text(objAnswerFood1[event.currentTarget.id])
+//Programming Answer Buttons
+        $('#submit').text(objAnswerProgramming[event.currentTarget.id])
+        $('#submit1').text(objAnswerProgramming1[event.currentTarget.id])
+//Math Answer Buttons
+        $('#submit').text(objAnswerMath[event.currentTarget.id])
+        $('#submit1').text(objAnswerMath1[event.currentTarget.id])
+//History Answer Buttons
+        $('#submit').text(objAnswerHistory[event.currentTarget.id])
+        $('#submit1').text(objAnswerHistory1[event.currentTarget.id])
+//States Answer Buttons
+        $('#submit').text(objAnswerStates[event.currentTarget.id])
+        $('#submit1').text(objAnswerStates1[event.currentTarget.id])
         }) 
         
         
+
+
+
+
+
 // creation of the modal for the questions when clicked
 
     const $modal = $('.modal');
     const $closeBtn = $('.close-btn');
     const $submitBtn = $('#submit');
+    const $submitBtn1 = $('#submit1');
     const $inputText = $('#inputText');
 
     $closeBtn.on('click', () => {
@@ -92,6 +183,13 @@ $(() => {
     })
 
     $submitBtn.on('click', () => {
+        if ($inputText == true) {
+            alert('Correct!');
+        } else {
+            alert('incorrect');
+        }  
+    })
+    $submitBtn1.on('click', () => {
         if ($inputText == true) {
             alert('Correct!');
         } else {
